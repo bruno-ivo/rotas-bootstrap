9ed870ccfe31092a1ce0a926b496ab2222629992
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-10 16:02:48 -0300

    CanLoad

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,21 +1,32 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree} from "@angular/router";
 import {Observable} from "rxjs";
 import {AuthService} from "../login/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanLoad{
 
   constructor(private authService: AuthService, private router: Router) { }
 
+
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso(){
     if (this.authService.usuarioEstaAutenticado()){
       return true;
     } else{
       this.router.navigate(['/login']);
       return false;
     }
+  }
+
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.verificarAcesso();
   }
 }
